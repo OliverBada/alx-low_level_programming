c646097439132680cc37e101644fcdfdd1250c3e
OliverBada <badaoliver7@gmail.com>
2022-07-01 13:39:16 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = '1'; ch <= '9'; ch++)
+	putchar('ch\n');
+	return (0);
+}
