ecb95332df5ee46362855595be1eaa1676cbada7
OliverBada <badaoliver7@gmail.com>
2022-07-01 14:42:40 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: o is success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 16; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
