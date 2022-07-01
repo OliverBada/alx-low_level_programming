ac56de5f321ee01190591c93c731bb44e446d542
OliverBada <badaoliver7@gmail.com>
2022-07-01 14:27:44 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+
+	while (z <= a)
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+
+}
