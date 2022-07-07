cc2f9831d9cd304985c2efd037d7d874572356dd
OliverBada <badaoliver7@gmail.com>
2022-07-07 02:25:59 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - fn to check for lowercase character
+ * @c: parameter c
+ * Return: Return 1 if c is a digit, return 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
