5cfa102dc870ee1a2447b40118dfff864b594d7d
OliverBada <badaoliver7@gmail.com>
2022-07-07 08:40:52 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main void
+{
+
+	int a = 1;
+
+	while (a <= 100)
+	if (a % 3 == 0)
+	{
+		printf(" Fizz\n");
+		a++;
+	}
+	else if (a % 5 == 0)
+	{
+		printf(" Buzz");
+		a++;
+	}
+	else
+	{
+		printf(" %d", a);
+		a++;
+	}
+	return (0);
+}
