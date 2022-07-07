bf4204274b8828c14a238af270dfaad2e2485581
OliverBada <badaoliver7@gmail.com>
2022-07-07 03:06:24 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - fn to print numbers
+* Return: 0 is success
+*/
+void print_numbers(void)
+{
+	int c;
+
+	if (c >= '0' && c <= '9')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
