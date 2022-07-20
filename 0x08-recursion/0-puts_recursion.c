d6aff6912dc27cb29d52c5542fdf30c4b5e2b2dc
OliverBada <badaoliver7@gmail.com>
2022-07-20 03:51:00 -0700

    She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts_recursion - Prints the strings
+*@s: string to be printed
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0' || *s == '0')
+	{
+	_putchar ('\n');
+	return;
+	}
+	_putchar (*s);
+	++s;
+	_puts_recursion(s);
+}
